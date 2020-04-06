4264c9103405019ce9036404a182fad4d7a94ee2
Paul downing <pauldowning@pauls-mbp.lan>
2020-04-06 15:30:01 -0500

    add people and contact infos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+paul = Person.create(
+  first_name: 'Paul',
+  last_name: 'Downing',
+  notes: 'prefers to be contacted via email',
+  is_agent: false
+)
+
+paul.contact_infos.create(
+  phone_one_label: 'mobile',
+  phone_one: '555-444-3333',
+  phone_two_label: 'work',
+  phone_two: '999-888-7777',
+  primary_email: 'charlie@work.edu',
+  secondary_email: 'charlie@home.edu'
+)
