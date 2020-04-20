641a69810849cb116fbcbda247f1f4472130697b
Paul downing <pauldowning@pauls-mbp.lan>
2020-04-20 17:54:38 -0500

    first wave of person cards

--- client/src/utils/view-utils.js
+++ client/src/utils/view-utils.js
@@ -17,3 +17,28 @@ export function formatState(stateCode) {
 export function formattedAddress(address) {
   return `${address.address}, ${address.city}, ${address.state}, ${address.zipCode}, ${address.country}`
 }
+
+export function fullName(person) {
+  const firstName = person.firstName
+  const lastName = person.lastName
+
+  if (firstName && lastName) {
+    return `${person.firstName} ${person.lastName}`
+  } else if (firstName && !lastName) {
+    return person.firstName
+  } else {
+    return "NAME MISSING"
+  }
+}
+
+export function setPersonType(person) {
+  if(person.isBuyer) {
+    return "Buyer"
+  } else if (person.isSeller) {
+    return "Seller"
+  } else if (person.isAgent) {
+    return "Agent"
+  } else {
+    return undefined
+  }
+}
